39b7fbb263597ec42160eec176551555ef9c3a0d
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2013-11-20 18:45:17 -0600

    Baseline

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,72 @@
 Travel::Application.routes.draw do
+  # Routes for the Photo resource:
+  # CREATE
+  get '/photos/new', controller: 'photos', action: 'new', as: 'new_photo'
+  post '/photos', controller: 'photos', action: 'create', as: 'photos'
+
+  # READ
+  get '/photos', controller: 'photos', action: 'index'
+  get '/photos/:id', controller: 'photos', action: 'show', as: 'photo'
+
+  # UPDATE
+  get '/photos/:id/edit', controller: 'photos', action: 'edit', as: 'edit_photo'
+  patch '/photos/:id', controller: 'photos', action: 'update'
+
+  # DELETE
+  delete '/photos/:id', controller: 'photos', action: 'destroy'
+  #------------------------------
+
+  # Routes for the Reply resource:
+  # CREATE
+  get '/replies/new', controller: 'replies', action: 'new', as: 'new_reply'
+  post '/replies', controller: 'replies', action: 'create', as: 'replies'
+
+  # READ
+  get '/replies', controller: 'replies', action: 'index'
+  get '/replies/:id', controller: 'replies', action: 'show', as: 'reply'
+
+  # UPDATE
+  get '/replies/:id/edit', controller: 'replies', action: 'edit', as: 'edit_reply'
+  patch '/replies/:id', controller: 'replies', action: 'update'
+
+  # DELETE
+  delete '/replies/:id', controller: 'replies', action: 'destroy'
+  #------------------------------
+
+  # Routes for the Request resource:
+  # CREATE
+  get '/requests/new', controller: 'requests', action: 'new', as: 'new_request'
+  post '/requests', controller: 'requests', action: 'create', as: 'requests'
+
+  # READ
+  get '/requests', controller: 'requests', action: 'index'
+  get '/requests/:id', controller: 'requests', action: 'show', as: 'request'
+
+  # UPDATE
+  get '/requests/:id/edit', controller: 'requests', action: 'edit', as: 'edit_request'
+  patch '/requests/:id', controller: 'requests', action: 'update'
+
+  # DELETE
+  delete '/requests/:id', controller: 'requests', action: 'destroy'
+  #------------------------------
+
+  # Routes for the User resource:
+  # CREATE
+  get '/users/new', controller: 'users', action: 'new', as: 'new_user'
+  post '/users', controller: 'users', action: 'create', as: 'users'
+
+  # READ
+  get '/users', controller: 'users', action: 'index'
+  get '/users/:id', controller: 'users', action: 'show', as: 'user'
+
+  # UPDATE
+  get '/users/:id/edit', controller: 'users', action: 'edit', as: 'edit_user'
+  patch '/users/:id', controller: 'users', action: 'update'
+
+  # DELETE
+  delete '/users/:id', controller: 'users', action: 'destroy'
+  #------------------------------
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
